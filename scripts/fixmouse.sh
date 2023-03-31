05da6196a708c3469fb6626787a2f46652d8c225
giulpig <giulio.codutti20@gmail.com>
2023-03-31 11:03:54 +0200

    fixed fixmouse and wii is now 16/9

--- scripts/fixmouse.sh
+++ scripts/fixmouse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xinput set-button-map 10 1 1 3 4 5 6 7
+sudo modprobe -r psmouse
+sudo modprobe psmouse
--- scripts/wii_4_3.sh
+++ scripts/wii_4_3.sh
@@ -1,1 +1,1 @@
-ffplay /dev/video2 -standard pal -fast -vf "eq=gamma=0.65:saturation=1.4:contrast=0.8" & ffplay -f alsa hw:2 -autoexit -nodisp -af 'volume=0.1'
+ffplay /dev/video2 -standard pal -fast -vf "scale=1920:1080,eq=gamma=0.65:saturation=1.4:contrast=0.8" & ffplay -f alsa hw:2 -autoexit -nodisp -af 'volume=0.1'

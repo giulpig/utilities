b67dc9df195dce384d243a3afc9d1e69f171ec72
giulpig <giulio.codutti20@gmail.com>
2023-03-31 11:08:58 +0200

    added useful new scripts

--- scripts/noti.sh
+++ scripts/noti.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# Info sul sistema in stile notifica (chi ha bisogno di una status bar?)
-# Riccardo Palombo - https://riccardo.im
-# Preparato per la community Patreon: patreon.com/riccardopalombo
 
-pkill herbe & \
-herbe "$(date +'%d/%m/%y %R')" " "\
- 		" "\
+pkill gstuff & \
+gstuff --noti "$(date +'%d/%m/%y %R')" " "\
       "$(set -- $(sensors | grep -i core | tr -d "+"); printf '%s\n' "CPU: $4")" " "\
-      "Batteria0: $(cat /sys/class/power_supply/BAT0/capacity)%" " "\
+      "Batteria0: $(cat /sys/class/power_supply/BAT0/capacity)%"\
       "Batteria1: $(cat /sys/class/power_supply/BAT1/capacity)%" " "\
-			"Schermo: $(light | cut -f1 -d'.')%"
+      "Schermo: $(light | cut -f1 -d'.')%"\
+      "Volume:  $(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))"
--- scripts/wii_4_3.sh
+++
@@ -1,1 +0,0 @@
-ffplay /dev/video2 -standard pal -fast -vf "scale=1920:1080,eq=gamma=0.65:saturation=1.4:contrast=0.8" & ffplay -f alsa hw:2 -autoexit -nodisp -af 'volume=0.1'
